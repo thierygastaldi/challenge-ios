644b925794036e570128747cc718cd508200fe85
Thiery <thiery@tech.fit>
2019-09-24 23:03:23 -0300

    First commit.

---
+++ ALodjinha/ALodjinha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ALodjinha
+//
+//  Created by Thiery Gastaldi on 24/09/19.
+//  Copyright © 2019 tgr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
